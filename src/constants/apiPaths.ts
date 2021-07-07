37a6a76642cc2a61f96bef201ef184440f32844e
Serhii_Kondrashenko <Serhii_Kondrashenko@epam.com>
2021-07-07 11:09:28 +0300

    task3 Added integration with backend api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://qsnju9olj2.execute-api.eu-west-1.amazonaws.com/shop',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
