600c9b684356a947e1328d82ad60abb7fc327d32
Serhii_Kondrashenko <Serhii_Kondrashenko@epam.com>
2021-09-29 12:21:11 +0300

    task7 added import url path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://0fxzvnjz7e.execute-api.us-east-1.amazonaws.com/shop',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ytrgnn0n5h.execute-api.us-east-1.amazonaws.com/shop',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
