423d611a2052886ceaf782a1e9dfcee822028e4f
Serhii_Kondrashenko <Serhii_Kondrashenko@epam.com>
2021-10-03 22:34:59 +0300

    task7 changed api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://0fxzvnjz7e.execute-api.us-east-1.amazonaws.com/shop',
+  product: 'https://8q3sqb1wxf.execute-api.us-east-1.amazonaws.com/shop',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ytrgnn0n5h.execute-api.us-east-1.amazonaws.com/shop',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
