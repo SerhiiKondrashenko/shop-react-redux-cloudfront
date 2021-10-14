6a03fef10d92c77bc854288cc22cc4a2051d2132
Kondrashenko <serhii.kondrashenko@refinitiv.com>
2021-10-14 13:27:10 +0300

    task8 added cart api to apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ytrgnn0n5h.execute-api.us-east-1.amazonaws.com/shop',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://serhiikondrashenko-cart-api-develop.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
